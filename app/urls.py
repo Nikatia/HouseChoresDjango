fe7d96c1fb5114ebfbbc7cf33b062ee49696c037
Monika Fredriksson <moni.fredriksson@gmail.com>
2023-05-22 12:09:02 +0300

    Chores and Categories done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from .views import landingview, loginview, login_action, logout_action, \
-                   chore_type_listview, add_chore_type, confirm_delete_chore_type, delete_chore_type, edit_chore_type_get, edit_chore_type_post
+                   chore_type_listview, add_chore_type, confirm_delete_chore_type, delete_chore_type, edit_chore_type_get, edit_chore_type_post, \
+                   chore_listview, add_chore, search_chores, confirm_delete_chore, delete_chore, edit_chore_get, edit_chore_post, chores_of_type
 
 urlpatterns = [
     #Main page
@@ -22,6 +23,14 @@ urlpatterns = [
     path('edit-chore-type-post/<int:id>/', edit_chore_type_post),
 
     #Chores
+    path('chores/', chore_listview),
+    path('add-chore/', add_chore),
+    path('search-chores/', search_chores),
+    path('confirm-delete-chore/<int:id>/', confirm_delete_chore),
+    path('delete-chore/<int:id>/', delete_chore),
+    path('edit-chore-get/<int:id>/', edit_chore_get),
+    path('edit-chore-post/<int:id>/', edit_chore_post),
+    path('category-chores/<int:id>/', chores_of_type)
 
     #History
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .models import ChoreType
+from .models import ChoreType, Chore
 
 # Main page
 def landingview(request):
@@ -49,7 +49,8 @@ def confirm_delete_chore_type(request, id):
         return render(request, 'loginpage.html')
     else:
         type = ChoreType.objects.get(id = id)
-        context = {'type': type}
+        chorelist = Chore.objects.all()
+        context = {'type': type, 'chores': chorelist}
         return render (request, "confirm_delete_chore_type.html", context)
 
 def delete_chore_type(request, id):
@@ -77,4 +78,78 @@ def edit_chore_type_post(request, id):
         type.save()
         return redirect(chore_type_listview)
 
+
+#Chores
+def chore_listview(request):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        chorelist = Chore.objects.all()
+        choretypelist = ChoreType.objects.all()
+        context = {'chores': chorelist, 'types': choretypelist}
+        return render(request, 'chores.html', context)
+
+def search_chores(request):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        search = request.POST['search']
+        filtered = Chore.objects.filter(name__icontains=search)
+        choretypelist = ChoreType.objects.all()
+        context = {'chores': filtered, 'types': choretypelist}
+        return render (request, "chores.html", context)
+
+def add_chore(request):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        name = request.POST['name']
+        type = request.POST['type']
+        Chore(name = name, type = ChoreType.objects.get(id = type)).save()
+        return redirect(request.META['HTTP_REFERER'])
+
+def confirm_delete_chore(request, id):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        chore = Chore.objects.get(id = id)
+        context = {'chore': chore}
+        return render (request, "confirm_delete_chore.html", context)
+
+def delete_chore(request, id):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        Chore.objects.get(id = id).delete()
+        return redirect(chore_listview)
+    
+def edit_chore_get(request, id):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        chore = Chore.objects.get(id = id)
+        choretypelist = ChoreType.objects.all()
+        context = {'chore': chore, 'types': choretypelist}
+        return render (request, "edit_chore.html", context)
+
+def edit_chore_post(request, id):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        chore = Chore.objects.get(id = id)
+        chore.name = request.POST['name']
+        type = request.POST['type']
+        chore.type = ChoreType.objects.get(id = int(type))
+        chore.save()
+        return redirect(chore_listview)
+
+def chores_of_type(request, id):
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        type = ChoreType.objects.get(id = id)
+        chorelist = Chore.objects.all()
+        filteredchores = chorelist.filter(type = id)
+        context = {'chores': filteredchores, 'type': type}
+        return render (request, "categorys_chores.html", context)
 
